69b15f89535ca4b7078be5bd606722150d78e45b
YousifTheKind <yousifmoeltayeb@gmail.com>
2024-09-03 21:11:49 +0300

    Implemented "append" method to the list

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
-import "./styles.css";
+#!/usr/bin/env node
+
+class LinkedList {
+    head = null;
+    append(value) {
+        if (this.head == null) {
+            this.head = new Node(value, this.head);
+        } else {
+            let temp = this.head;
+            while (temp.nextNode != null) {
+                temp = temp.nextNode;
+            }
+            temp.nextNode = new Node(value);
+        }
+    }
+}
+
+class Node {
+    value = null;
+    nextNode = null;
+
+    constructor(value, nextNode) {
+        this.value = value;
+        this.nextNode = nextNode;
+    }
+}
+
+const list = new LinkedList();
+list.append("ss");
+list.append("sh");
+list.append("cats");
+console.log(list.head);
